05323bef19ec4dd845c7e3a4ff02c75d5ce11fd6
Dave Rolsky <autarch@urth.org>
2025-11-01 13:41:30 -0500

    Create a distro for testing my GitHub Actions perl workflow

---
+++ lib/Workflow/Testing.pm
@@ -0,0 +1,10 @@
+package Workflow::Testing;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+# ABSTRACT: Testing module for workflow-perl-dzil-ci
---
+++ t/test.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+ok( 1, 'just need a test' );
+
+done_testing();
